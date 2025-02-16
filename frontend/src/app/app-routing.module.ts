84075440897c859d22f9beea6b0f450af13f43cf
kharon <kharon.harrell@outlook.com>
2025-02-15 23:32:15 -0800

    added landing signup and sign in

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent }, // Home page
+  { path: 'signup', component: SignupComponent }, // Sign-Up
+  { path: 'login', component: LoginComponent }, // Login
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LucideAngularModule, icons } from 'lucide-angular';
+import { LandingComponent } from './pages/landing/landing.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LandingComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LucideAngularModule.pick(icons)
   ],
   providers: [],
   bootstrap: [AppComponent]
